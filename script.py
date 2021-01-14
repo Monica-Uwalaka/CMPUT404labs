5aeaffb82ed1cc1697917ebc4533787bf89adae7
Student <muwalaka@ualberta.ca>
2021-01-13 22:19:02 -0500

    second commit

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 
 print(requests.__version__)
 
-req = requests.get('https://www.google.com')
+req = requests.get('https://raw.githubusercontent.com/Monica-Uwalaka/CMPUT404labs/main/script.py')
 
 print(req.text)
 
